ab33705dbddb519365d280ef454f2f65dece2c12
akash sutariya <akash.officials@gmail.com>
2023-05-15 10:04:39 +0530

    Create UI for Popup Modal

---
+++ src/components/ui/Modal.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full">
+    <div class="relative top-20 mx-auto p-5 border w-fit shadow-lg rounded-md bg-white">
+      <slot name="content" />
+    </div>
+  </div>
+</template>
